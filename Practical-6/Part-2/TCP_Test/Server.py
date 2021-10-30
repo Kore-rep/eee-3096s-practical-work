be3df4a27278e66eacb5b85d16283ba397747efb
Angus Longmore <lngang002@myuct.ac.za>
2021-10-30 10:06:16 +0100

    Working localhost server-client setup with sensor data

--- Practical-6/Part-2/TCP_Test/Server.py
+++ Practical-6/Part-2/TCP_Test/Server.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 
-import socket
+import socket, time
 
 print("Starting...")
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
 print("Creating and binding socket...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 print("Done.")
+
+print("Waiting for client..")
+s.listen(1)
+conn, addr = s.accept()
+print ('Connection address:', addr)
 try:
-    print("Waiting for client..")
-    s.listen(1)
-    conn, addr = s.accept()
-    print ('Connection address:', addr)
     while 1:
         data = conn.recv(BUFFER_SIZE)
-        if not data: break
+        if not data: continue
         print ("received data:", data.decode())
         conn.send(data)  # echo
+except KeyboardInterrupt:
+    conn.close()
 finally:
     conn.close()
--- Practical-6/Part-2/TCP_Test/light-and-temp.py
+++ Practical-6/Part-2/TCP_Test/light-and-temp.py
@@ -7,20 +7,23 @@ import threading
 import time
 import RPi.GPIO as GPIO;
 import socket
-from datetime import time
+from datetime import datetime
 
 def networkingSetup():
+    print("Network setup...")
     TCP_IP = '127.0.0.1'
     TCP_PORT = 5005
-
+    global BUFFER_SIZE
     BUFFER_SIZE = 1024
-    MESSAGE = "Hell, World!"
+    MESSAGE = "Hello, World!"
     global networkSocket
     networkSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     networkSocket.connect((TCP_IP, TCP_PORT))
+    print("Done.")
 
 
 def setup():
+    print("Board setup...")
     # create the spi bus
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -36,16 +39,11 @@ def setup():
 
     global samplingTimes, currentSamplingTime
     samplingTimes = [10, 5 , 1]
-    currentSamplingTime = 0
+    currentSamplingTime = 1
 
     GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.add_event_detect(17, GPIO.FALLING, callback = changeSamplingTime, bouncetime = 200)
-
-    TCP_IP = '127.0.0.1'
-    TCP_PORT = 5005
-
-    BUFFER_SIZE = 1024
-    MESSAGE = "Hell, World!"
+    print("Done.")
 
 def changeSamplingTime(channel):
     global samplingTimes, currentSamplingTime
@@ -57,11 +55,13 @@ def readValues():
     while True:
         sleepTime = samplingTimes[currentSamplingTime]
         #print(f"{str(round(time.time()-startTime))}s\t{str(temp.value)}\t\t{str(round((temp.voltage-0.5)/0.01, 2))}°C\t{str(light.value)}")
-        timestamp = time.strftime("%H:%M:%S")
+        now = datetime.now()
+        timestamp = now.strftime("%H:%M:%S")
         temp_value = str(temp.value)
         #temp_volts = str(round((temp.voltage-0.5)/0.01, 2))
         light_val = str(light.value)
         data_str = f"SENSORS#{timestamp}${temp_value}${light_val}"
+        print(data_str)
         networkSocket.send(data_str.encode())
         time.sleep(sleepTime)
 
@@ -69,10 +69,11 @@ if __name__ == '__main__':
     setup()
     networkingSetup()
     print("Runtime\tTemp Reading\tTemp\tLight Reading")
-    global startTime, networkSocket
-    startTime = time.time()
+    global startTime, networkSocket, BUFFER_SIZE
+    #startTime = time.time()
     x = threading.Thread(target=readValues)
     x.start()
     while 1:
-        data = networkSocket.recv()
+        data = networkSocket.recv(BUFFER_SIZE)
+    print(data.decode())
     x.join()
