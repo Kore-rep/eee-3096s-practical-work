9d013b9b5195a98f984533680316b674ef76d80b
Angus Longmore <lngang002@myuct.ac.za>
2021-10-31 11:46:14 +0200

    Updated client for networking, fixed requirements issue

--- Practical-6/Part-2/Client/src/light-and-temp.py
+++ Practical-6/Part-2/Client/src/light-and-temp.py
@@ -6,9 +6,29 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 import threading
 import time
 import RPi.GPIO as GPIO
+import socket
+from datetime import datetime
+
+
+def networkingSetup():
+    """Initialize sockets and connect to them"""
+    print("Network setup...")
+    TCP_IP = "102.39.247.225"
+    TCP_PORT = 5005
+
+    global BUFFER_SIZE, ENABLED
+    ENABLED = True
+    BUFFER_SIZE = 1024
+    MESSAGE = "Hello, World!"
+    global networkSocket
+    networkSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    networkSocket.connect((TCP_IP, TCP_PORT))
+    print("Done.")
 
 
 def setup():
+    """Initialize spi interface, GPIO pins and add interrupts"""
+    print("Board setup...")
     # create the spi bus
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -22,15 +42,22 @@ def setup():
     temp = AnalogIn(mcp, MCP.P1)
     light = AnalogIn(mcp, MCP.P2)
 
-    global samplingTimes, currentSamplingTime
+    global samplingTimes, currentSamplingTime, lastSampleTime
     samplingTimes = [10, 5, 1]
-    currentSamplingTime = 0
+    currentSamplingTime = 1
+    lastSampleTime = datetime.now()
 
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(17, GPIO.FALLING, callback=changeSamplingTime, bouncetime=200)
+    print("Done.")
 
 
 def changeSamplingTime(channel):
+    """Cycle between 1, 5 and 10 second sampling intervals.
+
+    :param channel: Required param for this method to act as an interupt
+    :type channel: n/a
+    """
     global samplingTimes, currentSamplingTime
     print(
         f"Sampling time will be changed from {samplingTimes[currentSamplingTime]}s to {samplingTimes[(currentSamplingTime + 1) % 3]}s after the next sample"
@@ -39,20 +66,71 @@ def changeSamplingTime(channel):
 
 
 def readValues():
-    global temp, light, samplingTimes, currentSamplingTime
-    while True:
+    """Read the value from the temperature sensor and ldr, compile them into a data packet and send them to the server."""
+    global temp, light, samplingTimes, currentSamplingTime, networkSocket, ENABLED, lastSampleTime
+    while ENABLED:
         sleepTime = samplingTimes[currentSamplingTime]
-        print(
-            f"{str(round(time.time()-startTime))}s\t{str(temp.value)}\t\t{str(round((temp.voltage-0.5)/0.01, 2))}°C\t{str(light.value)}"
-        )
+        # print(f"{str(round(time.time()-startTime))}s\t{str(temp.value)}\t\t{str(round((temp.voltage-0.5)/0.01, 2))}°C\t{str(light.value)}")
+        now = datetime.now() + datetime.timedelta(hours=2)
+        timestamp = now.strftime("%H:%M:%S")
+        tempValue = str(temp.value)
+        # temp_volts = str(round((temp.voltage-0.5)/0.01, 2))
+        lightVal = str(light.value)
+        dataStr = f"SENSORS#{timestamp}${tempValue}${lightVal}"
+        print(dataStr)
+        networkSocket.send(dataStr.encode())
+        lastSampleTime = now
         time.sleep(sleepTime)
 
 
+def sendStatus():
+    """Compile a timestamp and the status of the client into a datapack and send to server."""
+    global ENABLED, lastSampleTime, networkSocket
+    print("Sending Status...")
+    timestamp = lastSampleTime.strftime("%H:%M:%S")
+    dataStr = f"STATUS#{timestamp}${ENABLED}"
+    networkSocket.send(dataStr.encode())
+
+
+def disableSensors():
+    """Disable the sensor reading and sending. Send new status to server."""
+    global ENABLED
+    ENABLED = False
+    sendStatus()
+
+
+def enableSensors():
+    """Enable sensor reading and sending. Send new status to server."""
+    global ENABLED
+    ENABLED = True
+    sendStatus()
+
+
 if __name__ == "__main__":
+
+    # Run setups
     setup()
-    print("Runtime\tTemp Reading\tTemp\tLight Reading")
-    global startTime
-    startTime = time.time()
+    networkingSetup()
+    global startTime, networkSocket, BUFFER_SIZE
+
+    # Create and start threads to read and send sensor values
     x = threading.Thread(target=readValues)
     x.start()
-    x.join()
+    try:
+        while 1:
+
+            # Wait to recieve a packet
+            data = networkSocket.recv(BUFFER_SIZE)
+            serverMessage = data.decode()
+
+            # Decode packet and act accordingly
+            if serverMessage == "CHECK":
+                sendStatus()
+            elif serverMessage == "SENDOFF":
+                disableSensors()
+            elif serverMessage == "SENDON":
+                enableSensors()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        x.join()
