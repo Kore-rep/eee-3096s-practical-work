dd6644e94058eb302aa6d5a518c9dc9c04c26352
Malcolm <BTJMAL001@myuct.ac.za>
2021-10-31 14:47:58 +0200

    Fix for client connection flag not working consistently. added data folder for data recordings

--- Practical-6/Part-2/Server/src/main.py
+++ Practical-6/Part-2/Server/src/main.py
@@ -32,14 +32,15 @@ def serve_gui():
 @app.route('/download')
 def downloadFile():
     currentDate = datetime.now().strftime("%d%m%Y")
-    path = currentDate + '.csv'
+    path = f'data/{currentDate}.csv'
     return send_file(path, as_attachment=True)
 
 def setup():
-    global TCP_IP, TCP_PORT, BUFFER_SIZE, sensorOn, sensorReadings, clientConnected
+    global TCP_IP, TCP_PORT, BUFFER_SIZE, sensorOn, sensorReadings, clientConnected, logsDate
     sensorOn = True
     clientConnected = False
     sensorReadings=[""]
+    logsDate = ""
     # TCP_IP = '41.114.231.2'
     TCP_IP = '0.0.0.0'
     TCP_PORT = 5005
@@ -57,6 +58,8 @@ def sendCommand(command):
         data = command.encode()
         print(command)
         conn.send(data)
+        if (logsDate == "Client not connected"):
+            logsDate = ""
         clientConnected = True
         if (not sensorOn):
             data = conn.recv(BUFFER_SIZE)
@@ -84,13 +87,15 @@ def runServer():
         if (sensorOn):
             data = conn.recv(BUFFER_SIZE)
             if not data: break
+            if (logsDate == "Client not connected"):
+                logsDate = ""
             clientConnected = True
             print(data.decode())
             checkSensor = data.decode().split("#")
             if (checkSensor[0] == "SENSORS"):
                 currentDate = datetime.now().strftime("%d%m%y")
                 try:
-                    logFile = open(f'{currentDate}.csv', 'a')
+                    logFile = open(f'data/{currentDate}.csv', 'a')
                     sensorReadings = checkSensor[1].split("$")
                     logFile.write(sensorReadings[0] + ',' + sensorReadings[1] + ',' + sensorReadings[2] + '\n')
                 finally:
@@ -98,7 +103,8 @@ def runServer():
             if (checkSensor[0] == "STATUS"):
                 lastChecked = checkSensor[1].split("$")[0]
                 sensorOn = eval(checkSensor[1].split("$")[1])
-        conn.close()
+
+    conn.close()
 
 def checkLog():
     global sensorReadings, logsDate
@@ -107,7 +113,7 @@ def checkLog():
     while ((dateOffset <= 5) and (fileFound == False)):
         currentDate = datetime.now() - timedelta(days=dateOffset)
         try:
-            logFile = open(f'{currentDate.strftime("%d%m%y")}.csv', 'r')
+            logFile = open(f'data/{currentDate.strftime("%d%m%y")}.csv', 'r')
             sensorReadings = logFile.readlines()[-10:]
             for i in range(len(sensorReadings)):
                 tempReading = sensorReadings[i].split(",")
@@ -124,9 +130,8 @@ def checkLog():
 
 if __name__ == '__main__':
     setup()
-    global lastChecked, logsDate
+    global lastChecked, s
     lastChecked = (datetime.now() + timedelta(hours=2)).strftime("%H:%M:%S")
-    logsDate = ""
 
     appThread = threading.Thread(target=runApp)
     serverThread = threading.Thread(target=runServer)
@@ -136,3 +141,5 @@ if __name__ == '__main__':
     sendCommand("CHECK")
     appThread.join()
     serverThread.join()
+
+    s.close()
